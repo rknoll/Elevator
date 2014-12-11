84f9308970ddbd8bcb9a8a5c179ed54439d384d6
Richard Knoll <rknoll@me.com>
2014-12-11 10:34:55 +0100

    example main class

---
+++ src/main/java/at/fhhagenberg/sqe/project/Elevator.java
@@ -0,0 +1,19 @@
+/**
+ * @author Richard Knoll - S1310567017
+ */
+
+package at.fhhagenberg.sqe.project;
+
+/**
+ * Main Entry Class for the Elevator Project
+ */
+public class Elevator {
+
+    /**
+     * Main Entry function to the Elevator Project
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

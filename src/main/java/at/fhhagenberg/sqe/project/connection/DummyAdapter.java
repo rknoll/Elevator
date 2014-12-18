56a043e46e6561bd7c9f18e616c305ae33bb56a5
s1310567026@students.fh-hagenberg.at <s1310567026@students.fh-hagenberg.at>
2014-12-18 21:44:57 +0100

    DummyAdapter.java hinzugefügt: Erstellt einen Adapter mit Elevators und Floors. Die Funktionen sind implementiert.
    BuildingTest.java hinzugefügt: Erzeugt ein Building mit dem DummyAdapter.

---
+++ src/main/java/at/fhhagenberg/sqe/project/connection/DummyAdapter.java
@@ -0,0 +1,158 @@
+package at.fhhagenberg.sqe.project.connection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sqlelevator.IElevator;
+import at.fhhagenberg.sqe.project.model.Elevator;
+import at.fhhagenberg.sqe.project.model.Elevator.Direction;
+import at.fhhagenberg.sqe.project.model.Elevator.DoorStatus;
+import at.fhhagenberg.sqe.project.model.Floor;
+
+/**
+ * Created by Thomas Zöchbauer on 18/12/14.
+ */
+public class DummyAdapter implements IElevatorAdapter 
+{	
+	private List<Floor> mFloors;
+	private List<Elevator> mElevators;
+	
+	private int mNumElevators;
+	private int mNumFloors;
+	private int mFloorHeight;
+	
+	public DummyAdapter(int numElevators, int numFloors)
+	{
+		mNumElevators = numElevators;
+		mNumFloors = numFloors;
+		mFloorHeight = 9;	// feet
+		
+		mFloors = new ArrayList<Floor>();
+		mElevators = new ArrayList<Elevator>();
+		
+		for (int i = 0; i < mNumFloors; i++)
+		{
+			mFloors.add(new Floor(i, "Floor " + i ));
+		}
+		
+		for (int i = 0; i < mNumElevators; i++)
+		{
+			mElevators.add(new Elevator(i, "E"+i, mFloors));
+		}		
+	}
+	
+	@Override
+	public Direction getCommittedDirection(int elevatorNumber) throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getDirection();
+	}
+	
+	@Override
+	public int getElevatorAccel(int elevatorNumber)	throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getAcceleration();
+	}
+
+	@Override
+	public boolean getElevatorButton(int elevatorNumber, int floor)	throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getButton(mFloors.get(floor));
+	}
+
+	@Override
+	public DoorStatus getElevatorDoorStatus(int elevatorNumber)	throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getDoorStatus();
+	}
+
+	@Override
+	public int getElevatorFloor(int elevatorNumber)	throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getCurrentFloor().getFloorNumber();
+	}
+
+	@Override
+	public int getElevatorNum() throws ElevatorConnectionLostException {
+		return mNumElevators;
+	}
+
+	@Override
+	public int getElevatorPosition(int elevatorNumber) throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getPosition();
+	}
+
+	@Override
+	public int getElevatorSpeed(int elevatorNumber)	throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getSpeed();
+	}
+
+	@Override
+	public int getElevatorWeight(int elevatorNumber) throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getWeight();
+	}
+
+	@Override
+	public int getElevatorCapacity(int elevatorNumber) throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getCapacity();
+	}
+
+	@Override
+	public boolean getFloorButtonDown(int floor) throws ElevatorConnectionLostException {
+		return mFloors.get(floor).isButtonDown();
+	}
+
+	@Override
+	public boolean getFloorButtonUp(int floor) throws ElevatorConnectionLostException {
+		return mFloors.get(floor).isButtonUp();
+	}
+
+	@Override
+	public int getFloorHeight() throws ElevatorConnectionLostException {
+		return mFloorHeight;
+	}
+
+	@Override
+	public int getFloorNum() throws ElevatorConnectionLostException {
+		return mNumFloors;
+	}
+
+	@Override
+	public boolean getServicesFloors(int elevatorNumber, int floor)	throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getService(mFloors.get(floor));
+	}
+
+	@Override
+	public int getTarget(int elevatorNumber) throws ElevatorConnectionLostException {
+		return mElevators.get(elevatorNumber).getTarget().getFloorNumber();
+	}
+
+	@Override
+	public void setCommittedDirection(int elevatorNumber, int direction) throws ElevatorConnectionLostException {
+		Direction dir;
+		switch(direction)
+		{
+		case IElevator.ELEVATOR_DIRECTION_UP:
+			dir = Direction.UP;
+			break;
+		case IElevator.ELEVATOR_DIRECTION_DOWN:
+			dir = Direction.DOWN;
+			break;
+		case IElevator.ELEVATOR_DIRECTION_UNCOMMITTED:
+		default:
+			dir = Direction.UNCOMMITTED;	
+		}
+		
+		mElevators.get(elevatorNumber).setDirection(dir);	
+	}
+
+	@Override
+	public void setServicesFloors(int elevatorNumber, int floor, boolean service) throws ElevatorConnectionLostException {
+		mElevators.get(elevatorNumber).setService(mFloors.get(floor), service);		
+	}
+
+	@Override
+	public void setTarget(int elevatorNumber, int target) throws ElevatorConnectionLostException {
+		mElevators.get(elevatorNumber).setTarget(mFloors.get(target));		
+	}
+
+	@Override
+	public long getClockTick() throws ElevatorConnectionLostException {
+		return 0;
+	}
+
+}
---
+++ src/test/java/at/fhhagenberg/sqe/project/model/BuildingTest.java
@@ -0,0 +1,99 @@
+package at.fhhagenberg.sqe.project.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import org.junit.Before;
+import org.junit.Test;
+
+import at.fhhagenberg.sqe.project.connection.DummyAdapter;
+import at.fhhagenberg.sqe.project.connection.IElevatorAdapter;
+import at.fhhagenberg.sqe.project.services.listeners.IElevatorListener;
+
+/**
+ * Created by Thomas Zöchbauer on 18/12/14.
+ */
+public class BuildingTest {
+	
+	private Building building;
+	
+	@Before
+	public void setUp() throws Exception {
+		int numElevators = 2;
+		int numFloors = 5;
+		IElevatorAdapter adapter = new DummyAdapter(numElevators, numFloors);	
+		
+		building = new Building(adapter);
+	}
+
+	@Test
+	public void testGetElevators() {
+		Iterable<Elevator> elevatorIterable = building.getElevators();
+		Iterator<Elevator> elevatorIter = elevatorIterable.iterator();
+		
+		int elevCount = 0;
+		while (elevatorIter.hasNext())
+		{
+			Elevator e = elevatorIter.next();
+			elevCount++;
+		}
+		assertEquals(2, elevCount);		
+	}
+
+	@Test
+	public void testGetFloors() {
+		Iterable<Floor> floorIterable = building.getFloors();
+		Iterator<Floor> floorIter = floorIterable.iterator();
+		
+		int floorCount = 0;
+		while (floorIter.hasNext())
+		{
+			Floor f = floorIter.next();
+			floorCount++;
+		}
+		assertEquals(5, floorCount);	
+	}
+
+	@Test
+	public void testAddAndRemoveListener() {
+		IElevatorListener listener = new IElevatorListener() {
+			@Override
+			public void update() {
+				// do nothing
+			}
+		};
+		building.addListener(listener);
+		building.removeListener(listener);
+	}
+
+	@Test
+	public void testRemoveAllListeners() {
+		IElevatorListener listener = new IElevatorListener() {
+			@Override
+			public void update() {
+				// do nothing
+			}
+		};
+		IElevatorListener listener2 = new IElevatorListener() {
+			@Override
+			public void update() {
+				// do nothing
+			}
+		};
+		
+		building.addListener(listener);
+		building.addListener(listener2);
+		building.removeAllListeners();
+	}
+
+	@Test
+	public void testGetNumberOfElevators() {
+		assertEquals(2, building.getNumberOfElevators());
+	}
+
+	@Test
+	public void testGetNumberOfFloors() {
+		assertEquals(5, building.getNumberOfFloors());
+	}
+
+}

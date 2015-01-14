1cb1202b8f7f29f421601885db894166c67cc0c2
Richard Knoll <rknoll@me.com>
2015-01-14 16:02:38 +0100

    fixed tests in eclipse

--- src/test/java/at/fhhagenberg/sqe/project/ElevatorLauncherTest.java
+++ src/test/java/at/fhhagenberg/sqe/project/ElevatorLauncherTest.java
@@ -1,19 +1,15 @@
 package at.fhhagenberg.sqe.project;
 
-import at.fhhagenberg.sqe.project.model.Building;
+import at.fhhagenberg.sqe.mocks.TestConfiguration;
 import at.fhhagenberg.sqe.project.services.IUpdateThread;
-import at.fhhagenberg.sqe.project.services.model.BuildingServiceTest;
 import at.fhhagenberg.sqe.project.ui.IElevatorWindow;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
@@ -23,8 +19,6 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by rknoll on 07/01/15.
  */
-@Configuration
-@ComponentScan(value = {"at.fhhagenberg.sqe.project.mocks"})
 public class ElevatorLauncherTest {
     private AnnotationConfigApplicationContext context;
 
@@ -38,7 +32,7 @@ public class ElevatorLauncherTest {
 
     @Before
     public void setUp() throws Exception {
-        context = new AnnotationConfigApplicationContext(ElevatorLauncherTest.class);
+        context = new AnnotationConfigApplicationContext(TestConfiguration.class);
         AutowireCapableBeanFactory acbFactory = context.getAutowireCapableBeanFactory();
         acbFactory.autowireBean(this);
         launcher = new ElevatorLauncher(context);

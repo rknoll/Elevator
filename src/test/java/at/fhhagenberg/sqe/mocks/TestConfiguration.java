51e4475754e795e46a81bd48b357739f338c7773
Richard Knoll <rknoll@me.com>
2015-01-14 16:03:45 +0100

    added test config for eclipse

---
+++ src/test/java/at/fhhagenberg/sqe/mocks/TestConfiguration.java
@@ -0,0 +1,9 @@
+package at.fhhagenberg.sqe.mocks;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"at.fhhagenberg.sqe.mocks"})
+public class TestConfiguration {
+}
